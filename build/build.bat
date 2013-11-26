0a0ac511d2c3b8d08fb08a58521c553348ddf756
Gavin Jones <gavin.jones.nz@gmail.com>
2013-11-27 00:59:53 +1300

    version bump

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 
 %MSBUILD% Build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=60
-%MSBUILD% build-sn.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=1
+%MSBUILD% build-sn.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=60
